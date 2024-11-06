d8ed6f351f7c955ebb6f72ae715170ad3fed1f78
Paweł Kieszonkowski <pawloo38@gmail.com>
2024-11-06 20:25:30 +0100

    moduł 0

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char varClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(varClass);
+        System.out.println(isGoodProgrammer);
+
+        String hellow = "Hello from FirstClass!";
+        System.out.println(hellow);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String firstName = "Jan";
+        String lastName = "Kowalski";
+        int age = 22;
+        double weight = 95;
+        double height = 1.85;
+        String drivingLicenceCategory = "B+E";
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(drivingLicenceCategory);
+    }
+}
