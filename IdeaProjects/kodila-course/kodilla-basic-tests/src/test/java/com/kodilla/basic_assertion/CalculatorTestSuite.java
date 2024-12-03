37c0e13929fff6e2b6ec5973fb5d963ac035d63a
Paweł Kieszonkowski <pawloo38@gmail.com>
2024-12-03 22:11:55 +0100

    zadanie3.6

--- IdeaProjects/kodila-course/kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ IdeaProjects/kodila-course/kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -13,4 +13,37 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subtractResult = calculator.subtract(b, a);
+        assertEquals(3, subtractResult);
+    }
+
+    @Test
+    public void testPowerOfTwoNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        int a = -3;
+        int powerOfTwoResult1 = calculator.powerOfTwo(a);
+        assertEquals(9, powerOfTwoResult1);
+    }
+
+    @Test
+    public void testPowerOfTwoZero() {
+        Calculator calculator = new Calculator();
+        int b = 0;
+        int powerOfTwoResult2 = calculator.powerOfTwo(b);
+        assertEquals(0, powerOfTwoResult2);
+    }
+
+    @Test
+    public void testPowerOfTwoPositiveNumbers() {
+        Calculator calculator = new Calculator();
+        int c = 4;
+        int powerOfTwoResult3 = calculator.powerOfTwo(c);
+        assertEquals(16, powerOfTwoResult3);
+    }
 }
