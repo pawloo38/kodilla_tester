b73d4db2fb5460ace404234ce0e5530f8b333199
Paweł Kieszonkowski <pawloo38@gmail.com>
2024-12-27 13:05:39 +0100

    zadanie 5.3

---
+++ IdeaProjects/kodila-course/kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/BookHacked.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.immutable;
+
+public class BookHacked extends Book {
+    public BookHacked(String author, String title) {
+        super(author, title);
+    }
+    public void modifyTitle(String newTitle) {
+        title = newTitle;
+    }
+}
