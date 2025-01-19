42abb0fc3ce6a7dfc0ff73989f215535873e1ef9
Paweł Kieszonkowski <pawloo38@gmail.com>
2025-01-19 20:40:04 +0100

    aktualizacja zadanie 7.6

--- IdeaProjects/kodila-course/kodilla-advanced-tests/src/test/java/com/kodilla/notification/homework/WeatherNotificationServiceTest.java
+++ IdeaProjects/kodila-course/kodilla-advanced-tests/src/test/java/com/kodilla/notification/homework/WeatherNotificationServiceTest.java
@@ -2,6 +2,8 @@ package com.kodilla.notification.homework;
 
 import com.kodilla.notification.Client;
 import com.kodilla.notification.Notification;
+import com.kodilla.notification.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -9,13 +11,25 @@ import static org.mockito.Mockito.*;
 
 class WeatherNotificationServiceTest {
 
-    WeatherNotificationService weatherNotificationService = new WeatherNotificationService();
-    Client client = Mockito.mock(Client.class);
-    Client secondClient = Mockito.mock(Client.class);
-    Client thirdClient = Mockito.mock(Client.class);
-    Location location = new Location("Kraków");
-    Location secondLocation = new Location("Warszawa");
-    Notification notification = new WeatherNotification("Zagrożenie gradem");
+    private WeatherNotificationService weatherNotificationService;
+    private Client client;
+    private Client secondClient;
+    private Client thirdClient;
+    private Notification notification;
+    private Location location;
+    private Location secondLocation;
+
+    @BeforeEach
+    public void setup() {
+        weatherNotificationService = new WeatherNotificationService();
+        NotificationService notificationService = new NotificationService();
+        client = Mockito.mock(Client.class);
+        secondClient = Mockito.mock(Client.class);
+        thirdClient = Mockito.mock(Client.class);
+        notification = Mockito.mock(Notification.class);
+        location = new Location("Kraków");
+        secondLocation = new Location("Warszawa");
+    }
 
     @Test
     public void notSubscribedClientShouldNotReceiveLocationNotification() {
@@ -26,9 +40,7 @@ class WeatherNotificationServiceTest {
     @Test
     public void subscribedClientShouldReceiveLocationNotification() {
         weatherNotificationService.addSubscriberToLocation(client, location);
-
         weatherNotificationService.sendNotificationToLocation(notification, location);
-
         verify(client).receive(notification);
     }
 
@@ -37,9 +49,7 @@ class WeatherNotificationServiceTest {
         weatherNotificationService.addSubscriberToLocation(client, location);
         weatherNotificationService.addSubscriberToLocation(secondClient, secondLocation);
         weatherNotificationService.addSubscriberToLocation(thirdClient, location);
-
         weatherNotificationService.sendNotificationToAll(notification);
-
         verify(client).receive(notification);
         verify(secondClient).receive(notification);
         verify(thirdClient).receive(notification);
@@ -49,9 +59,7 @@ class WeatherNotificationServiceTest {
     public void shouldSendOnlyOneNotificationToMultiTimeSubscriber() {
         weatherNotificationService.addSubscriberToLocation(client, location);
         weatherNotificationService.addSubscriberToLocation(client, secondLocation);
-
         weatherNotificationService.sendNotificationToAll(notification);
-
         verify(client, times(1)).receive(notification);
     }
 
@@ -59,9 +67,7 @@ class WeatherNotificationServiceTest {
     public void unsubscribedClientShouldNotReceiveLocationNotification() {
         weatherNotificationService.addSubscriberToLocation(client, location);
         weatherNotificationService.removeSubscriberFromLocation(client, location);
-
         weatherNotificationService.sendNotificationToLocation(notification, location);
-
         verify(client, never()).receive(notification);
     }
 
@@ -69,12 +75,9 @@ class WeatherNotificationServiceTest {
     public void shouldRemoveClientFromAllLocations() {
         weatherNotificationService.addSubscriberToLocation(client, location);
         weatherNotificationService.addSubscriberToLocation(client, secondLocation);
-
         weatherNotificationService.removeAllSubscriptions(client);
-
         weatherNotificationService.sendNotificationToLocation(notification, location);
         weatherNotificationService.sendNotificationToLocation(notification, secondLocation);
-
         verify(client, never()).receive(notification);
     }
 
@@ -82,11 +85,8 @@ class WeatherNotificationServiceTest {
     public void shouldRemoveLocationAndItsSubscribers() {
         weatherNotificationService.addSubscriberToLocation(client, location);
         weatherNotificationService.addSubscriberToLocation(secondClient, location);
-
         weatherNotificationService.removeLocation(location);
-
         weatherNotificationService.sendNotificationToLocation(notification, location);
-
         verify(client, never()).receive(notification);
         verify(secondClient, never()).receive(notification);
     }
