c25396f41153c4bfab32a9d25f5b22efb538d79f
Paweł Kieszonkowski <pawloo38@gmail.com>
2025-01-27 23:48:42 +0100

    aktualizacja zadania 9.2

--- IdeaProjects/kodila-course/kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
+++ IdeaProjects/kodila-course/kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -12,19 +12,19 @@ public class Calculator {
     }
 
     public double add(double a, double b) {
-        double result = a + b;
+        double result = Math.addExact((int)a, (int)b);
         display.display(result);
         return result;
     }
 
     public double subtract(double a, double b) {
-        double result = a - b;
+        double result = Math.subtractExact((int)a, (int)b);
         display.display(result);
         return result;
     }
 
     public double multiply(double a, double b) {
-        double result = a * b;
+        double result = Math.multiplyExact((int)a, (int)b);
         display.display(result);
         return result;
     }
